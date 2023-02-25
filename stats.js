7c37f51b20f9dab610bb548bd9198ab316c9ec81
Jeon Niroumand <jeon.niroumand@gmail.com>
2023-02-24 22:00:20 -0800

    added 24 hour prices

--- stats.js
+++ stats.js
@@ -1,8 +1,26 @@
-const xmlhttp = new XMLHttpRequest;
-xmlhttp.onload = function() {
-    const trtlPrice = JSON.parse(this.responseText);
-    document.getElementById("trtl_price").innerHTML = trtlPrice.turtlecoin.usd;
-};
-xmlhttp.open("GET", "https://api.coingecko.com/api/v3/simple/price?ids=turtlecoin&vs_currencies=usd&precision=18");
-xmlhttp.send();
+coins = [`wrapped-turtlecoin`];
+twentyFourHoursAgo = (Math.round(Date.now()/1000)) - 86400; //return epoch time 24 hours ago
+now = Math.round(Date.now()/1000); //return epoch time
 
+const coin_usd = new XMLHttpRequest;
+coin_usd.onload = function() {
+    const wtrtlPrice = JSON.parse(this.responseText);
+    document.getElementById("wtrtl_price").innerHTML = wtrtlPrice[coins].usd; // return price in usd
+}
+coin_usd.open("GET", "https://api.coingecko.com/api/v3/simple/price?ids=wrapped-turtlecoin&vs_currencies=usd&precision=18");
+coin_usd.send();
+
+const oneDayPrices = new XMLHttpRequest;
+oneDayPrices.onload = function() {
+    const coinPrices = JSON.parse(this.responseText);
+    console.log(coinPrices.prices)//return list of prices
+}
+oneDayPrices.open("GET", `https://api.coingecko.com/api/v3/coins/wrapped-turtlecoin/market_chart/range?vs_currency=usd&from=${twentyFourHoursAgo}&to=${now}`)
+oneDayPrices.send();
+
+for (coin in coins) {
+    console.log(coins[coin]);
+}
+
+console.log(twentyFourHoursAgo);
+console.log(now);
